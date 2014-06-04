2fc35de1567be3decb0909be9390d0f112ac03c4
SlowRiot <rain.backnet@gmail.com>
2014-06-04 14:30:24 +0100

    placeholder voronoi chunk setup

--- chunk.cpp
+++ chunk.cpp
@@ -4,6 +4,7 @@
 #include <cmath>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <voro++.hh>
 #include "entity.h"
 #include "universe.h"
 #include "world.h"
@@ -104,134 +105,30 @@ void chunk::setup() {
   std::vector<buffer_chunk::vertex> vbodata;
   std::vector<GLuint>               ibodata;
 
-  // menger sponge
-  unsigned int constexpr iters = 2;
-  unsigned int constexpr numblocks = static_cast<int>(pow(3.0, static_cast<double>(iters)));
-  float constexpr blocksize = size / static_cast<float>(numblocks) / 2.0;
+  // voronoi triangulation
+  vbodata.reserve(100);   // make sure to reserve the correct size to avoid re-allocations during construction
+  ibodata.reserve(100);
 
-  vbodata.reserve(6 * 4 * numblocks * numblocks * numblocks);   // make sure to reserve the correct size to avoid re-allocations during construction
-  ibodata.reserve(6 * 6 * numblocks * numblocks * numblocks);
 
-  Vector3f const normal0( 0.0, 0.0, -1.0);
-  Vector3f const normal1(-1.0, 0.0,  0.0);
-  Vector3f const normal2( 0.0, 1.0,  0.0);
+  // Create a container with the geometry given above, and make it
+  // non-periodic in each of the three coordinates. Allocate space for
+  // eight particles within each computational block
+  voro::container con(0.0, size,              // the minimum and maximum x coordinates
+                      0.0, size,              // the minimum and maximum y coordinates
+                      0.0, size,              // the minimum and maximum z coordinates
+                      6, 6, 6,                // the number of grid blocks in each of the three coordinate directions
+                      false, false, false,    // flags setting whether the container is periodic in each coordinate direction - see http://math.lbl.gov/voro++/doc/refman/classvoro_1_1container.html#a50aaf382a069b102930b88976215818f
+                      8);                     // the initial memory allocation for each block (number of particles)
 
-  for(unsigned int x = 0; x != numblocks; ++x) {
-    for(unsigned int y = 0; y != numblocks; ++y) {
-      for(unsigned int z = 0; z != numblocks; ++z) {
-        //blocksize = static_cast<int>(pow(3.0, static_cast<double>(iters))) * x/static_cast<float>(numblocks) * y/static_cast<float>(numblocks) * z/static_cast<float>(numblocks);
-        bool skip = false;
-        int depth = 1;
-        for(unsigned int i = 0; i != iters; ++i) {
-          int matches = 0;
-          if((x / depth) % 3 == 1) {
-            ++matches;
-          }
-          if((y / depth) % 3 == 1) {
-            ++matches;
-          }
-          if((z / depth) % 3 == 1) {
-            ++matches;
-          }
-          if(matches >= 2) {
-            skip = true;
-            break;
-          }
-          depth *= 3;
-        }
-        if(skip) {
-          continue;
-        }
-        Vector3f const coord000(blocksize * x, blocksize * y, blocksize * z);
-        Vector3f const coord100(coord000 + Vector3f(blocksize, 0.0f, 0.0f));
-        Vector3f const coord010(coord000 + Vector3f(0.0f, blocksize, 0.0f));
-        Vector3f const coord110(coord000 + Vector3f(blocksize, blocksize, 0.0f));
-
-        Vector3f const coord001(blocksize * x, blocksize * y, blocksize * z + blocksize);
-        Vector3f const coord101(coord001 + Vector3f(blocksize, 0.0f, 0.0f));
-        Vector3f const coord011(coord001 + Vector3f(0.0f, blocksize, 0.0f));
-        Vector3f const coord111(coord001 + Vector3f(blocksize, blocksize, 0.0f));
-
-        // front face
-        unsigned int offset = vbodata.size();
-        vbodata.emplace_back(coord000, normal0);
-        vbodata.emplace_back(coord010, normal0);
-        vbodata.emplace_back(coord110, normal0);
-        vbodata.emplace_back(coord100, normal0);
-        ibodata.emplace_back(offset + 0);
-        ibodata.emplace_back(offset + 1);
-        ibodata.emplace_back(offset + 2);
-        ibodata.emplace_back(offset + 2);
-        ibodata.emplace_back(offset + 3);
-        ibodata.emplace_back(offset + 0);
-
-        // back face
-        offset = vbodata.size();
-        vbodata.emplace_back(coord001, -normal0);
-        vbodata.emplace_back(coord011, -normal0);
-        vbodata.emplace_back(coord111, -normal0);
-        vbodata.emplace_back(coord101, -normal0);
-        ibodata.emplace_back(offset + 0);
-        ibodata.emplace_back(offset + 3);
-        ibodata.emplace_back(offset + 2);
-        ibodata.emplace_back(offset + 2);
-        ibodata.emplace_back(offset + 1);
-        ibodata.emplace_back(offset + 0);
-
-        // Side face 1
-        offset = vbodata.size();
-        vbodata.emplace_back(coord000, normal1);
-        vbodata.emplace_back(coord001, normal1);
-        vbodata.emplace_back(coord011, normal1);
-        vbodata.emplace_back(coord010, normal1);
-        ibodata.emplace_back(offset + 0);
-        ibodata.emplace_back(offset + 1);
-        ibodata.emplace_back(offset + 2);
-        ibodata.emplace_back(offset + 2);
-        ibodata.emplace_back(offset + 3);
-        ibodata.emplace_back(offset + 0);
-
-        // Side face 2
-        offset = vbodata.size();
-        vbodata.emplace_back(coord100, -normal1);
-        vbodata.emplace_back(coord101, -normal1);
-        vbodata.emplace_back(coord111, -normal1);
-        vbodata.emplace_back(coord110, -normal1);
-        ibodata.emplace_back(offset + 0);
-        ibodata.emplace_back(offset + 3);
-        ibodata.emplace_back(offset + 2);
-        ibodata.emplace_back(offset + 2);
-        ibodata.emplace_back(offset + 1);
-        ibodata.emplace_back(offset + 0);
-
-        // top face
-        offset = vbodata.size();
-        vbodata.emplace_back(coord010, normal2);
-        vbodata.emplace_back(coord011, normal2);
-        vbodata.emplace_back(coord111, normal2);
-        vbodata.emplace_back(coord110, normal2);
-        ibodata.emplace_back(offset + 0);
-        ibodata.emplace_back(offset + 1);
-        ibodata.emplace_back(offset + 2);
-        ibodata.emplace_back(offset + 2);
-        ibodata.emplace_back(offset + 3);
-        ibodata.emplace_back(offset + 0);
-
-        // bottom face
-        offset = vbodata.size();
-        vbodata.emplace_back(coord000, -normal2);
-        vbodata.emplace_back(coord001, -normal2);
-        vbodata.emplace_back(coord101, -normal2);
-        vbodata.emplace_back(coord100, -normal2);
-        ibodata.emplace_back(offset + 0);
-        ibodata.emplace_back(offset + 3);
-        ibodata.emplace_back(offset + 2);
-        ibodata.emplace_back(offset + 2);
-        ibodata.emplace_back(offset + 1);
-        ibodata.emplace_back(offset + 0);
-      }
-    }
+  // Randomly add particles into the container
+  for(unsigned int i = 0; i != 20; ++i) {
+    float const x = float(rand()) / RAND_MAX * size;
+    float const y = float(rand()) / RAND_MAX * size;
+    float const z = float(rand()) / RAND_MAX * size;
+    con.put(i, x, y, z);
   }
+
+  std::cout << "DEBUG: Voronoi volume: " << con.sum_cell_volumes() << std::endl;
 
   vbodata.shrink_to_fit();
   ibodata.shrink_to_fit();
--- version.h
+++ version.h
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR  = 1;
 	static const long MINOR  = 5;
-	static const long BUILD  = 587;
-	static const long REVISION  = 3104;
+	static const long BUILD  = 596;
+	static const long REVISION  = 3165;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 1015;
-	#define RC_FILEVERSION 1,5,587,3104
-	#define RC_FILEVERSION_STRING "1, 5, 587, 3104\0"
-	static const char FULLVERSION_STRING [] = "1.5.587.3104";
+	static const long BUILDS_COUNT  = 1029;
+	#define RC_FILEVERSION 1,5,596,3165
+	#define RC_FILEVERSION_STRING "1, 5, 596, 3165\0"
+	static const char FULLVERSION_STRING [] = "1.5.596.3165";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 87;
+	static const long BUILD_HISTORY  = 96;
 	
 
 }
