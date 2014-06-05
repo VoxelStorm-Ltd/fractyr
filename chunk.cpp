4bd28d6038a08a2dd154d92a66aa109b61df7edc
John Turner <groups@norgg.org>
2014-06-05 23:47:34 +0100

    Spawn grunts in the center of the chunk to avoid load cascades.

--- chunk.cpp
+++ chunk.cpp
@@ -21,9 +21,9 @@ chunk::chunk(Vector3i const &chunk_coords, world &parent)
   /// Default constructor
 
   // generate enemies
-  float gruntscale = 1 + static_cast<float>((chunk_coords - Vector3i(world::size/2, world::size/2, world::size/2)).length()) / (world::size / 16);
+  float gruntscale = 1 + static_cast<float>((chunk_coords - Vector3i(world::size/2.0, world::size/2.0, world::size/2.0)).length()) / (world::size / 16);
   std::cout << "DEBUG: Spawning grunt with scale: " << gruntscale << std::endl;
-  grunt *grunt1 = new grunt(parent, this, Vector3f(70.0, 10.0, 50.0), Quatf::fromEulerAngles(0.0, 0.0, 0.0), gruntscale);
+  grunt *grunt1 = new grunt(parent, this, Vector3f(size/2.0, size/2.0, size/2.0), Quatf::fromEulerAngles(0.0, 0.0, 0.0), gruntscale);
   grunt1->add_weapon(new blaster(grunt1, 120.0  / gruntscale, 0.9 * gruntscale));
 
   setup_buffers();
