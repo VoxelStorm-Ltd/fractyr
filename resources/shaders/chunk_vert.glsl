24d5aede2b569d6e065eecf7ea9840ec474848d0
SlowRiot <rain.backnet@gmail.com>
2014-06-05 10:48:47 +0100

    adding colour attribute to chunk shader

--- resources/shaders/chunk_vert.glsl
+++ resources/shaders/chunk_vert.glsl
@@ -4,8 +4,10 @@
 
 attribute vec4 coords;    // we only input a vec3, so w defaults to 1.0
 attribute vec3 normal;
+attribute vec3 colour;
 
 varying vec3 normal_frag;
+varying vec3 colour_frag;
 varying vec3 reflectdir_frag;
 varying vec3 coords_camlocal_frag;
 
@@ -13,6 +15,7 @@ void main() {
   const vec3 lightdir = vec3(0.267261, 0.534522, 0.801784);
   reflectdir_frag = gl_NormalMatrix * reflect(lightdir, normal);
   normal_frag = normal;
+  colour_frag = colour;
 
   gl_Position = gl_ModelViewProjectionMatrix * coords;
   coords_camlocal_frag = (gl_ModelViewMatrix * coords).xyz;
--- version.h
+++ version.h
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR  = 1;
 	static const long MINOR  = 7;
-	static const long BUILD  = 732;
-	static const long REVISION  = 3919;
+	static const long BUILD  = 751;
+	static const long REVISION  = 4021;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 1259;
-	#define RC_FILEVERSION 1,7,732,3919
-	#define RC_FILEVERSION_STRING "1, 7, 732, 3919\0"
-	static const char FULLVERSION_STRING [] = "1.7.732.3919";
+	static const long BUILDS_COUNT  = 1293;
+	#define RC_FILEVERSION 1,7,751,4021
+	#define RC_FILEVERSION_STRING "1, 7, 751, 4021\0"
+	static const char FULLVERSION_STRING [] = "1.7.751.4021";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 32;
+	static const long BUILD_HISTORY  = 51;
 	
 
 }
