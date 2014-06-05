fca9decbea4d70acd6c121d760fac2bfcac0f3d1
SlowRiot <rain.backnet@gmail.com>
2014-06-05 06:52:21 +0100

    voronoi cubes

--- version.h
+++ version.h
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR  = 1;
 	static const long MINOR  = 6;
-	static const long BUILD  = 630;
-	static const long REVISION  = 3331;
+	static const long BUILD  = 649;
+	static const long REVISION  = 3429;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 1086;
-	#define RC_FILEVERSION 1,6,630,3331
-	#define RC_FILEVERSION_STRING "1, 6, 630, 3331\0"
-	static const char FULLVERSION_STRING [] = "1.6.630.3331";
+	static const long BUILDS_COUNT  = 1118;
+	#define RC_FILEVERSION 1,6,649,3429
+	#define RC_FILEVERSION_STRING "1, 6, 649, 3429\0"
+	static const char FULLVERSION_STRING [] = "1.6.649.3429";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 30;
+	static const long BUILD_HISTORY  = 49;
 	
 
 }
