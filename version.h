135a31032b05e3d63b119cd1f9fb5618941ca5c3
John Turner <groups@norgg.org>
2014-06-06 23:22:28 +0100

    Fix teleport bug due to double chunk changing.

--- version.h
+++ version.h
@@ -7,26 +7,27 @@ namespace AutoVersion{
 	static const char DATE[] = "06";
 	static const char MONTH[] = "06";
 	static const char YEAR[] = "2014";
-	static const char UBUNTU_VERSION_STYLE[] =  "14.06";
+	static const char UBUNTU_VERSION_STYLE[] = "14.06";
 	
 	//Software Status
-	static const char STATUS[] =  "Alpha";
-	static const char STATUS_SHORT[] =  "a";
+	static const char STATUS[] = "Alpha";
+	static const char STATUS_SHORT[] = "a";
 	
 	//Standard Version Type
-	static const long MAJOR  = 2;
-	static const long MINOR  = 2;
-	static const long BUILD  = 1318;
-	static const long REVISION  = 6977;
+	static const long MAJOR = 2;
+	static const long MINOR = 2;
+	static const long BUILD = 1331;
+	static const long REVISION = 7052;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 2446;
-	#define RC_FILEVERSION 2,2,1318,6977
-	#define RC_FILEVERSION_STRING "2, 2, 1318, 6977\0"
-	static const char FULLVERSION_STRING [] = "2.2.1318.6977";
+	static const long BUILDS_COUNT = 2474;
+	#define RC_FILEVERSION 2,2,1331,7052
+	#define RC_FILEVERSION_STRING "2, 2, 1331, 7052\0"
+	static const char FULLVERSION_STRING[] = "2.2.1331.7052";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 18;
+	static const long BUILD_HISTORY = 31;
+	
 
 }
 #endif //VERSION_H
