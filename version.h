e48e37805f5910eaf5af42a3fcda4495d847adcd
SlowRiot <rain.backnet@gmail.com>
2014-06-06 17:23:28 +0100

    optimising get_is_solid slightly

--- version.h
+++ version.h
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR  = 2;
 	static const long MINOR  = 1;
-	static const long BUILD  = 1219;
-	static const long REVISION  = 6491;
+	static const long BUILD  = 1225;
+	static const long REVISION  = 6525;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 2283;
-	#define RC_FILEVERSION 2,1,1219,6491
-	#define RC_FILEVERSION_STRING "2, 1, 1219, 6491\0"
-	static const char FULLVERSION_STRING [] = "2.1.1219.6491";
+	static const long BUILDS_COUNT  = 2295;
+	#define RC_FILEVERSION 2,1,1225,6525
+	#define RC_FILEVERSION_STRING "2, 1, 1225, 6525\0"
+	static const char FULLVERSION_STRING [] = "2.1.1225.6525";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 19;
+	static const long BUILD_HISTORY  = 25;
 	
 
 }
