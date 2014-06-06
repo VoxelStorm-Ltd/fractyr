7a38819a9f7b5683c764bd97a297919598536ef1
SlowRiot <rain.backnet@gmail.com>
2014-06-06 18:24:53 +0100

    constant voronoi cell density regardless of chunk size

--- version.h
+++ version.h
@@ -16,17 +16,18 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR  = 2;
 	static const long MINOR  = 1;
-	static const long BUILD  = 1231;
-	static const long REVISION  = 6563;
+	static const long BUILD  = 1243;
+	static const long REVISION  = 6622;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 2304;
-	#define RC_FILEVERSION 2,1,1231,6563
-	#define RC_FILEVERSION_STRING "2, 1, 1231, 6563\0"
-	static const char FULLVERSION_STRING [] = "2.1.1231.6563";
+	static const long BUILDS_COUNT  = 2325;
+	#define RC_FILEVERSION 2,1,1243,6622
+	#define RC_FILEVERSION_STRING "2, 1, 1243, 6622\0"
+	static const char FULLVERSION_STRING [] = "2.1.1243.6622";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 31;
+	static const long BUILD_HISTORY  = 43;
+	
 
 }
 #endif //VERSION_H
