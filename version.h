afb30947b0590a13d8ac89ff96ba81fa6ce4b83a
SlowRiot <rain.backnet@gmail.com>
2014-06-06 21:06:16 +0100

    nearer clip planes for a smaller world

--- version.h
+++ version.h
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR  = 2;
 	static const long MINOR  = 1;
-	static const long BUILD  = 1291;
-	static const long REVISION  = 6843;
+	static const long BUILD  = 1293;
+	static const long REVISION  = 6851;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 2414;
-	#define RC_FILEVERSION 2,1,1291,6843
-	#define RC_FILEVERSION_STRING "2, 1, 1291, 6843\0"
-	static const char FULLVERSION_STRING [] = "2.1.1291.6843";
+	static const long BUILDS_COUNT  = 2417;
+	#define RC_FILEVERSION 2,1,1293,6851
+	#define RC_FILEVERSION_STRING "2, 1, 1293, 6851\0"
+	static const char FULLVERSION_STRING [] = "2.1.1293.6851";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 91;
+	static const long BUILD_HISTORY  = 93;
 	
 
 }
