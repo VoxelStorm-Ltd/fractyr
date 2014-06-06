2c68ff5cebeafeeab0c99b0a98093400a2383238
SlowRiot <rain.backnet@gmail.com>
2014-06-06 19:57:33 +0100

    smaller world

--- version.h
+++ version.h
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR  = 2;
 	static const long MINOR  = 1;
-	static const long BUILD  = 1259;
-	static const long REVISION  = 6689;
+	static const long BUILD  = 1262;
+	static const long REVISION  = 6706;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 2353;
-	#define RC_FILEVERSION 2,1,1259,6689
-	#define RC_FILEVERSION_STRING "2, 1, 1259, 6689\0"
-	static const char FULLVERSION_STRING [] = "2.1.1259.6689";
+	static const long BUILDS_COUNT  = 2359;
+	#define RC_FILEVERSION 2,1,1262,6706
+	#define RC_FILEVERSION_STRING "2, 1, 1262, 6706\0"
+	static const char FULLVERSION_STRING [] = "2.1.1262.6706";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 59;
+	static const long BUILD_HISTORY  = 62;
 	
 
 }
