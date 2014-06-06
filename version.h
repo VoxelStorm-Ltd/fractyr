6bb3cbb103ab0c8a313691dca6e48c41bae58d27
SlowRiot <rain.backnet@gmail.com>
2014-06-06 20:42:26 +0100

    helper function to force an entity movement without checking collisions

--- version.h
+++ version.h
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR  = 2;
 	static const long MINOR  = 1;
-	static const long BUILD  = 1282;
-	static const long REVISION  = 6799;
+	static const long BUILD  = 1284;
+	static const long REVISION  = 6808;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 2400;
-	#define RC_FILEVERSION 2,1,1282,6799
-	#define RC_FILEVERSION_STRING "2, 1, 1282, 6799\0"
-	static const char FULLVERSION_STRING [] = "2.1.1282.6799";
+	static const long BUILDS_COUNT  = 2403;
+	#define RC_FILEVERSION 2,1,1284,6808
+	#define RC_FILEVERSION_STRING "2, 1, 1284, 6808\0"
+	static const char FULLVERSION_STRING [] = "2.1.1284.6808";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 82;
+	static const long BUILD_HISTORY  = 84;
 	
 
 }
