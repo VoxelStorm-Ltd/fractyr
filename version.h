fab7db50ab9535f819c24d83dcd1ba7d6aabb6d7
SlowRiot <slowriot@voxelstorm.com>
2015-06-25 15:55:20 +0100

    switching controls to work with joystick

--- version.h
+++ version.h
@@ -4,10 +4,10 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "31";
-	static const char MONTH[] = "01";
+	static const char DATE[] = "13";
+	static const char MONTH[] = "06";
 	static const char YEAR[] = "2015";
-	static const char UBUNTU_VERSION_STYLE[] =  "15.01";
+	static const char UBUNTU_VERSION_STYLE[] =  "15.06";
 	
 	//Software Status
 	static const char STATUS[] =  "Alpha";
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR  = 2;
 	static const long MINOR  = 2;
-	static const long BUILD  = 1372;
-	static const long REVISION  = 7289;
+	static const long BUILD  = 1377;
+	static const long REVISION  = 7326;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 2545;
-	#define RC_FILEVERSION 2,2,1372,7289
-	#define RC_FILEVERSION_STRING "2, 2, 1372, 7289\0"
-	static const char FULLVERSION_STRING [] = "2.2.1372.7289";
+	static const long BUILDS_COUNT  = 2561;
+	#define RC_FILEVERSION 2,2,1377,7326
+	#define RC_FILEVERSION_STRING "2, 2, 1377, 7326\0"
+	static const char FULLVERSION_STRING [] = "2.2.1377.7326";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 72;
+	static const long BUILD_HISTORY  = 77;
 	
 
 }
