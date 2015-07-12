1b741fc7434fb40f00a4e5da9c671668e5f755ed
SlowRiot <slowriot@voxelstorm.com>
2015-07-12 17:13:30 +0100

    Auto-update from libvoxelstorm: latest get_version from advertcity (32997e5)

--- get_version.cpp
+++ get_version.cpp
@@ -1,11 +1,13 @@
-#include <string>
+#include "get_version.h"
 #include "version.h"
 
-std::string get_version() {
+std::string const get_version() {
   /// Return a string describing the current version.
   /// This lives in its own file to minimise re-compilation times, as it's always recompiled
   std::string version(AutoVersion::STATUS);
-  version += " ";
+  if(version.length() != 0) {
+    version += " ";
+  }
   version += AutoVersion::FULLVERSION_STRING;
   return version;
 }
