19a2ca35c8793862a0c1fe48e2f30aeee6936f1e
Borealis <slowriot@voxelstorm.com>
2015-09-06 16:52:16 +0100

    Auto-update from libvoxelstorm: exception catching in the glfw error callback (351e9c3)

--- callback_error.cpp
+++ callback_error.cpp
@@ -2,5 +2,11 @@
 
 void callback_error(int error, char const *description) {
   /// GLFW error reporting callback function
-  std::cout << "ERROR: GLFW: " << error << ": " << description << std::endl;
+  try {
+    std::cout << "ERROR: GLFW: " << error << ": " << description << std::endl;
+  } catch(std::exception const &e) {
+    std::cout << "ERROR: GLFW reported an error and additionally the parsing callback produced an exception: " << e.what() << ", GLFW error code " << error << std::endl;
+  } catch(...) {
+    std::cout << "ERROR: GLFW reported an error and additionally the parsing callback produced an unknown exception, GLFW error code " << error << std::endl;
+  }
 }
