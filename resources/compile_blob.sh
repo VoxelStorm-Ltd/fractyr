b9ac57ed42b498ce1341db3e2b5e5a5586e86567
Borealis <slowriot@voxelstorm.com>
2016-04-16 17:05:50 +0100

    Auto-update from libvoxelstorm: more intelligent compiled binary blob handler script (965274b)

--- resources/compile_blob.sh
+++ resources/compile_blob.sh
@@ -6,12 +6,25 @@
 infile="$1"
 outfile="$2"
 compiler="$3"
+options="$4"
+link_options="$5"
+#echo "Infile: $infile (eol)"
+#echo "Outfile: $outfile (eol)"
+#echo "Compiler: $compiler (eol)"
+#echo "Options: $options (eol)"
+#echo "Link options: $link_options (eol)"
+
 if [ "$compiler" = "" ]; then
   compiler="g++5"
   if [ -z "$(which "$compiler")" ]; then
     compiler="g++"
   fi
 fi
+
+# convert absolute path to relative if it's a subdirectory
+pwd="$(pwd | sed 's/\//\\\//g')\/"
+infile="$(sed "s/$pwd//" <<< "$infile")"
+echo "Relative path $infile"
 
 if grep -iq ".glsl$" <<< "$infile"; then
   # additional validation step for glsl shaders
@@ -23,8 +36,12 @@ if grep -iq "apple" <<< "$MACHTYPE"; then
   # os x requires special treatment
   xxd -i "$infile" | sed 's/\(resources[a-zA-Z0-9_]*\)/_binary_\1_x/' | "$compiler" -c -o "$outfile" -x c++ -
 elif grep -iq "linux" <<< "$MACHTYPE"; then
-  # everything else just works with gnu ld
-  ld -r -b binary -o "$outfile" "$infile"
+  # everything else just works with gnu ld, but we need to select bitness
+  if grep -Fq -- '-m32' <<< "$options"; then
+    ld -m elf_i386 -r -b binary -o "$outfile" "$infile"
+  else
+    ld -r -b binary -o "$outfile" "$infile"
+  fi
 else
   # ...except if we're on windows, and then hoops need to be jumped through for backslashes
   outfile="$(sed 's/\\/\//g' <<< "$outfile")"
