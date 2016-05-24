d4aa0802d16da6b55079e88b5a6b8ac3808cad89
SlowRiot <slowriot@voxelstorm.com>
2016-05-24 21:06:23 +0100

    correct resource compiling commands in project

--- version.h
+++ version.h
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR  = 2;
 	static const long MINOR  = 3;
-	static const long BUILD  = 1435;
-	static const long REVISION  = 7655;
+	static const long BUILD  = 1438;
+	static const long REVISION  = 7663;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 2637;
-	#define RC_FILEVERSION 2,3,1435,7655
-	#define RC_FILEVERSION_STRING "2, 3, 1435, 7655\0"
-	static const char FULLVERSION_STRING [] = "2.3.1435.7655";
+	static const long BUILDS_COUNT  = 2640;
+	#define RC_FILEVERSION 2,3,1438,7663
+	#define RC_FILEVERSION_STRING "2, 3, 1438, 7663\0"
+	static const char FULLVERSION_STRING [] = "2.3.1438.7663";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 35;
+	static const long BUILD_HISTORY  = 38;
 	
 
 }
