71d1306436a695127834c5dfa7bb65b2a1564701
Borealis <slowriot@voxelstorm.com>
2016-05-25 22:00:34 +0100

    Auto-update from libvoxelstorm: windows-friendly modifications and better directory handling (a2c4b5b)

--- resources/upload_to_itchio.sh
+++ resources/upload_to_itchio.sh
@@ -9,25 +9,48 @@ if grep -iq "apple" <<< "$MACHTYPE"; then
   platform="osx-64"
   platform_cbx="Mac64_Release"
   butler=~/Library/Application\ Support/itch/bin/butler
+  if ! [ -f "$butler" ]; then
+    butler="resources/itchio_butler_mac64.exe"
+  fi
+  tempdir="$TMPDIR"
 elif grep -iq "linux" <<< "$MACHTYPE"; then
   # linux
   platform="linux-64"
   platform_cbx="Linux64_Release"
   butler=~/.config/itch/bin/butler
+  if ! [ -f "$butler" ]; then
+    butler="resources/itchio_butler_linux64.exe"
+  fi
+  tempdir="/dev/shm/temp_itchio"
 else
   # windows
   platform="windows-32"
   platform_cbx="Win32_Release"
-  butler=%APPDATA%/Roaming/itch/bin/butler.exe
+  butler="$APPDATA/Roaming/itch/bin/butler.exe"
+  if ! [ -f "$butler" ]; then
+    butler="resources/itchio_butler_win32.exe"
+  fi
+  tempdir="./temp_itchio"
 fi
 channel="$repo-for-$platform"
 
+if ! [ -f "$butler" ]; then
+  echo "Could not find itch.io butler on your system, expected it at $butler - cannot continue.  You need to install itch.io client or download the butler manually."
+  exit 1
+fi
+
+function get_online_version {
+  #wget -q -O - "https://itch.io/api/1/x/wharf/latest?target=voxelstorm/$repo&channel_name=$channel" \
+  #  --user-agent="VoxelStorm Game Uploader" \
+  #  | jq -r .latest
+  # compatible with windows toolchains:
+  curl -s "https://itch.io/api/1/x/wharf/latest?target=voxelstorm/$repo&channel_name=$channel" \
+    -A "VoxelStorm Game Uploader" \
+    | cut -d '"' -f 4
+}
+
 version=$(grep "FULLVERSION_STRING" "$repodir/version.h" | cut -d '"' -f 2)
-oldversion=$(
-  wget -q -O - "https://itch.io/api/1/x/wharf/latest?target=voxelstorm/$repo&channel_name=$channel" \
-    --user-agent="VoxelStorm Game Uploader" \
-    | jq -r .latest
-  )
+oldversion=$(get_online_version)
 
 echo "Existing version of $repo is $oldversion in $channel."
 if [ "$oldversion" == "$version" ]; then
@@ -35,10 +58,9 @@ if [ "$oldversion" == "$version" ]; then
   exit
 fi
 
-binpath=$(fgrep '<Target title="'"$platform_cbx"'">' *.cbp -A5 | fgrep 'Option output=' | cut -d '"' -f 2)
+binpath=$(grep -F '<Target title="'"$platform_cbx"'">' "$repodir/"*.cbp -A5 | grep -F 'Option output=' | head -1 | cut -d '"' -f 2)
 echo "Copying $binpath to temporary location..."
-tempdir="/dev/shm/temp_itchio"
-mkdir -p "$tempdir"
+mkdir -p "$tempdir" || exit 1
 cp "$repodir/$binpath" "$tempdir/" || exit 1
 echo "Uploading $repo version $version to $channel..."
 
@@ -48,11 +70,7 @@ rm -r "$tempdir"&
 
 echo "Verifying uploaded version..."
 sleep 1
-newversion=$(
-  wget -q -O - "https://itch.io/api/1/x/wharf/latest?target=voxelstorm/$repo&channel_name=$channel" \
-    --user-agent="VoxelStorm Game Uploader" \
-    | jq -r .latest
-  )
+newversion=$(get_online_version)
 
 if [ "$newversion" == "$version" ]; then
   echo "Uploaded version of $repo verified as $newversion."
