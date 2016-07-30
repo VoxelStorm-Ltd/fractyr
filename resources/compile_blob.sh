bc044a6be26088ca6c5a5d5f2739771c4993733e
Borealis <slowriot@voxelstorm.com>
2016-07-30 14:01:13 +0100

    Auto-update from libvoxelstorm: adapt de-parallelising process limit for resource compiler to number of logical cores available (f97b85f)

--- resources/compile_blob.sh
+++ resources/compile_blob.sh
@@ -47,7 +47,9 @@ if grep -iq "apple" <<< "$MACHTYPE"; then
 elif grep -iq "linux" <<< "$MACHTYPE"; then
   # de-parallelise blob compiles on linux to prevent OOM situations with high parallelism
   #process_limit=4
-  process_limit=3
+  #process_limit=3
+  process_limit=$(grep -c ^processor /proc/cpuinfo)
+  #((process_limit--))
   process_to_watch="xxd\|cc1plus"
   process_list=$(ps aux | grep "$process_to_watch")
   process_count=$(wc -l <<< "$process_list")
