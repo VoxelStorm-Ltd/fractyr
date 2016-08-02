d96ffe1dab992c392f6cf5eafce30501301095aa
Borealis <slowriot@voxelstorm.com>
2016-08-02 21:13:41 +0100

    Auto-update from libvoxelstorm: better handling in case the driver returns a zero sized long while compiling shaders, as happens with nvidia (40ab0b9)

--- shader_load.cpp
+++ shader_load.cpp
@@ -7,8 +7,8 @@ GLuint shader_load(std::string const &shader_vertex_source, std::string const &s
   char const *shader_vert_src_c = shader_vertex_source.c_str();
   char const *shader_frag_src_c = shader_fragment_source.c_str();
 
-  GLuint shader_vert = 0;                                                       // vertex shader
-  GLuint shader_frag = 0;                                                       // fragment shader
+  GLuint shader_vert    = 0;                                                    // vertex shader
+  GLuint shader_frag    = 0;                                                    // fragment shader
   GLuint shader_program = 0;                                                    // linked shader program
   shader_vert = glCreateShader(GL_VERTEX_SHADER);
   shader_frag = glCreateShader(GL_FRAGMENT_SHADER);
@@ -19,23 +19,34 @@ GLuint shader_load(std::string const &shader_vertex_source, std::string const &s
   glShaderSource(shader_vert, 1, &shader_vert_src_c, NULL);
   glCompileShader(shader_vert);
 
-  // Check vertex shader
-  GLint result = GL_FALSE;
-  GLint log_length = 0;
-  glGetShaderiv(shader_vert, GL_COMPILE_STATUS, &result);
-  glGetShaderiv(shader_vert, GL_INFO_LOG_LENGTH, &log_length);
-  std::vector<char> shader_vert_error(log_length);
-  glGetShaderInfoLog(shader_vert, log_length, NULL, &shader_vert_error[0]);
-  if(result == GL_TRUE) {
-    //std::cout << "done (" << shader_vert << ")." << std::endl;
-  } else {
-    std::cout << "failed, code " << result << ":" << std::endl;
-    std::cout << &shader_vert_error[0] << std::endl;
+  auto detach_all = [&]{
     glDetachShader(shader_program, shader_vert);                                // glDeleteShader won't work until the shaders have been detached
     glDetachShader(shader_program, shader_frag);
     glDeleteShader(shader_vert);
     glDeleteShader(shader_frag);
-    return GL_FALSE;
+  };
+
+  {
+    // Check vertex shader
+    GLint result = GL_FALSE;
+    glGetShaderiv(shader_vert, GL_COMPILE_STATUS, &result);
+    if(result == GL_TRUE) {
+      //std::cout << "done (" << shader_vert << ")." << std::endl;
+    } else {
+      GLint log_length = 0;
+      glGetShaderiv(shader_vert, GL_INFO_LOG_LENGTH, &log_length);
+      std::cout << "DEBUG: vert GL_COMPILE_STATUS " << result << " GL_INFO_LOG_LENGTH " << log_length << std::endl;
+      std::cout << "failed, code " << result;
+      if(log_length > 0) {
+        std::vector<char> shader_vert_error(log_length);
+        glGetShaderInfoLog(shader_vert, log_length, NULL, &shader_vert_error[0]); // only try to get a log if one is available
+        std::cout << ":" << std::endl << &shader_vert_error[0] << std::endl;
+      } else {
+        std::cout << "." << std::endl;
+      }
+      detach_all();
+      return GL_FALSE;
+    }
   }
 
   // Compile fragment shader
@@ -43,51 +54,59 @@ GLuint shader_load(std::string const &shader_vertex_source, std::string const &s
   std::cout << "fragment... ";
   glShaderSource(shader_frag, 1, &shader_frag_src_c, NULL);
   glCompileShader(shader_frag);
-
-  // Check fragment shader
-  glGetShaderiv(shader_frag, GL_COMPILE_STATUS, &result);
-  glGetShaderiv(shader_frag, GL_INFO_LOG_LENGTH, &log_length);
-  std::vector<char> shader_frag_error(log_length);
-  glGetShaderInfoLog(shader_frag, log_length, NULL, &shader_frag_error[0]);
-  if(result == GL_TRUE) {
-    //std::cout << "done (" << shader_frag << ")." << std::endl;
-  } else {
-    std::cout << "failed, code " << result << ":" << std::endl;
-    std::cout << &shader_frag_error[0] << std::endl;
-    glDetachShader(shader_program, shader_vert);                                // glDeleteShader won't work until the shaders have been detached
-    glDetachShader(shader_program, shader_frag);
-    glDeleteShader(shader_vert);
-    glDeleteShader(shader_frag);
-    return GL_FALSE;
+  {
+    // Check fragment shader
+    GLint result = GL_FALSE;
+    glGetShaderiv(shader_frag, GL_COMPILE_STATUS, &result);
+    if(result == GL_TRUE) {
+      //std::cout << "done (" << shader_frag << ")." << std::endl;
+    } else {
+      GLint log_length = 0;
+      glGetShaderiv(shader_frag, GL_INFO_LOG_LENGTH, &log_length);
+      std::cout << "DEBUG: frag GL_COMPILE_STATUS " << result << " GL_INFO_LOG_LENGTH " << log_length << std::endl;
+      std::cout << "failed, code " << result;
+      if(log_length > 0) {
+        std::vector<char> shader_frag_error(log_length);
+        glGetShaderInfoLog(shader_frag, log_length, NULL, &shader_frag_error[0]); // only try to get a log if one is available
+        std::cout << result << ":" << std::endl << &shader_frag_error[0] << std::endl;
+      } else {
+        std::cout << result << "." << std::endl;
+      }
+      detach_all();
+      return GL_FALSE;
+    }
   }
 
+  // Link the shader program
   //std::cout << "Shader: Linking program...           ";
   std::cout << "linking... ";
-  shader_program = glCreateProgram();
+  shader_program = glCreateProgram();                                           // create the shader program
   glAttachShader(shader_program, shader_vert);
   glAttachShader(shader_program, shader_frag);
   glLinkProgram(shader_program);
-
-  glGetProgramiv(shader_program, GL_LINK_STATUS, &result);
-  glGetProgramiv(shader_program, GL_INFO_LOG_LENGTH, &log_length);
-  std::vector<char> programError( (log_length > 1) ? log_length : 1 );
-  glGetProgramInfoLog(shader_program, log_length, NULL, &programError[0]);
-  if(result == GL_TRUE) {
-    std::cout << "done (" << shader_program << ")." << std::endl;
-  } else {
-    std::cout << "failed, code " << result << ":" << std::endl;
-    std::cout << &programError[0] << std::endl;
-    glDetachShader(shader_program, shader_vert);                                // glDeleteShader won't work until the shaders have been detached
-    glDetachShader(shader_program, shader_frag);
-    glDeleteShader(shader_vert);
-    glDeleteShader(shader_frag);
-    return GL_FALSE;
+  {
+    // Check link success
+    GLint result = GL_FALSE;
+    glGetProgramiv(shader_program, GL_LINK_STATUS, &result);
+    if(result == GL_TRUE) {
+      std::cout << "done (" << shader_program << ")." << std::endl;
+    } else {
+      GLint log_length = 0;
+      glGetProgramiv(shader_program, GL_INFO_LOG_LENGTH, &log_length);
+      std::cout << "DEBUG: link GL_COMPILE_STATUS " << result << " GL_INFO_LOG_LENGTH " << log_length << std::endl;
+      std::cout << "failed, code " << result;
+      if(log_length > 0) {
+        std::vector<char> program_error(log_length);
+        glGetProgramInfoLog(shader_program, log_length, NULL, &program_error[0]); // only try to get a log if one is available
+        std::cout << ":" << std::endl << &program_error[0] << std::endl;
+      } else {
+        std::cout << "." << std::endl;
+      }
+      detach_all();
+      return GL_FALSE;
+    }
   }
 
-  glDetachShader(shader_program, shader_vert);                                  // glDeleteShader won't work until the shaders have been detached
-  glDetachShader(shader_program, shader_frag);
-  glDeleteShader(shader_vert);
-  glDeleteShader(shader_frag);
-
+  detach_all();
   return shader_program;
 }
