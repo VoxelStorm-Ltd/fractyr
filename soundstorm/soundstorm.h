51d22b33c214886b34477a8bd448571637ab8046
Borealis <slowriot@voxelstorm.com>
2016-08-12 04:24:28 +0100

    Auto-update from libvoxelstorm: making constexprs const (4a4a956)

--- soundstorm/soundstorm.h
+++ soundstorm/soundstorm.h
@@ -128,13 +128,13 @@ private:
   unsigned int num_devices = 0;                                                 // number of known devices
   unsigned int channels = 2;                                                    // output channels
   float samplerate = 44100.0f;                                                  // output sample rate
-  static float constexpr speed_of_sound = 343.0f;                               // speed of sound in air, m/s
-  static float constexpr ear_offset = 0.115f;                                   // distance of ear from the centre of the head, metres
-  static float constexpr head_shadow_time = 0.000660f;                          // measured max pan head shadow time, seconds (see http://en.wikipedia.org/wiki/Interaural_time_difference#Duplex_theory)
+  static float constexpr const speed_of_sound = 343.0f;                         // speed of sound in air, m/s
+  static float constexpr const ear_offset = 0.115f;                             // distance of ear from the centre of the head, metres
+  static float constexpr const head_shadow_time = 0.000660f;                    // measured max pan head shadow time, seconds (see http://en.wikipedia.org/wiki/Interaural_time_difference#Duplex_theory)
   // how much extra time delay to add to the far sound for head shadow effect at full pan:
-  static float constexpr head_shadow_delay_max = head_shadow_time - (ear_offset / speed_of_sound);
+  static float constexpr const head_shadow_delay_max = head_shadow_time - (ear_offset / speed_of_sound);
   // how much the opposite ear is shadowed by the head, realistic ~= 6.4dB, we're going for somewhat of an exaggeration:
-  static float constexpr head_shadow_attenuation = 0.9f;
+  static float constexpr const head_shadow_attenuation = 0.9f;
   unsigned int num_decks = 2;                                                   // how many music decks we're currently using
   unsigned int deck_buffer_size = static_cast<unsigned int>(samplerate * 2.0f); // how many pcm frames to buffer for each deck buffer - this is the minimum pre-loaded at one time
 
