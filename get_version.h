4b45b5186541e6d161eec35a153d5c980d106819
Borealis <slowriot@voxelstorm.com>
2017-01-05 21:06:49 +0000

    Auto-update from libvoxelstorm: Auto-aligning comments (ee4c90a)

--- get_version.h
+++ get_version.h
@@ -1,9 +1,18 @@
 #ifndef GET_VERSION_H_INCLUDED
 #define GET_VERSION_H_INCLUDED
+#ifndef GET_VERSION_H_INCLUDED
 
+#define GET_VERSION_H_INCLUDED
 #include <string>
 
+
+#include <string>
 std::string const get_version();
+
+std::string const get_git_version();
+
+std::string const get_version();
+#endif // GET_VERSION_H_INCLUDED
 std::string const get_git_version();
 
 #endif // GET_VERSION_H_INCLUDED
