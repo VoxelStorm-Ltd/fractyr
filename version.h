f4861d48080c7bb4cf8167ba647526760824a004
SlowRiot <slowriot@voxelstorm.com>
2017-03-06 17:29:52 +0000

    removing boost and switching to std::filesystem

--- version.h
+++ version.h
@@ -4,10 +4,10 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "20";
-	static const char MONTH[] = "02";
+	static const char DATE[] = "06";
+	static const char MONTH[] = "03";
 	static const char YEAR[] = "2017";
-	static const char UBUNTU_VERSION_STYLE[] =  "17.02";
+	static const char UBUNTU_VERSION_STYLE[] =  "17.03";
 	
 	//Software Status
 	static const char STATUS[] =  "Alpha";
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR  = 2;
 	static const long MINOR  = 3;
-	static const long BUILD  = 1492;
-	static const long REVISION  = 7970;
+	static const long BUILD  = 1493;
+	static const long REVISION  = 7980;
 	
 	//Miscellaneous Version Types
 	static const long BUILDS_COUNT  = 2765;
-	#define RC_FILEVERSION 2,3,1492,7970
-	#define RC_FILEVERSION_STRING "2, 3, 1492, 7970\0"
-	static const char FULLVERSION_STRING [] = "2.3.1492.7970";
+	#define RC_FILEVERSION 2,3,1493,7980
+	#define RC_FILEVERSION_STRING "2, 3, 1493, 7980\0"
+	static const char FULLVERSION_STRING [] = "2.3.1493.7980";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 92;
+	static const long BUILD_HISTORY  = 93;
 	
 
 }
